ff73c66139da76ff2b55bd678c62bd684cc198fb
NguyenNam34 <khacnamdt@gmail.com>
2017-11-04 19:23:59 +0700

    [NamNK] Android Buoi 7  Bai2

--- app/src/main/java/com/example/sony/training/MainActivity.java
+++ app/src/main/java/com/example/sony/training/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.sony.training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button mButtonGoToActivitySecond,mButtonOpenGoogleDotCom,mButtonOpenGallery,
+                mButtonOpenCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initEvents();
+    }
+
+    private void initViews(){
+        mButtonGoToActivitySecond = (Button) findViewById(R.id.btnGoToActivity2);
+        mButtonOpenGoogleDotCom = (Button) findViewById(R.id.btnOpenGoogleDotCom);
+        mButtonOpenGallery = (Button) findViewById(R.id.btnOpenGallery);
+        mButtonOpenCamera = (Button) findViewById(R.id.btnOpenCamera);
+    }
+
+    private void initEvents(){
+        mButtonGoToActivitySecond.setOnClickListener(this);
+        mButtonOpenGoogleDotCom.setOnClickListener(this);
+        mButtonOpenGallery.setOnClickListener(this);
+        mButtonOpenCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnGoToActivity2:
+                openActivitySecond();
+                break;
+            case R.id.btnOpenGoogleDotCom:
+                break;
+            case R.id.btnOpenGallery:
+                break;
+            case R.id.btnOpenCamera:
+                break;
+        }
+    }
+
+    private void openActivitySecond(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/sony/training/SecondActivity.java
@@ -0,0 +1,8 @@
+package com.example.sony.training;
+
+/**
+ * Created by Administrator on 11/03/17.
+ */
+
+public class SecondActivity {
+}
